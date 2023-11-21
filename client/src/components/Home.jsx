1e554058c26f1eb1730b1f865bcb29eeddfc89c0
loc-giang <locbgiang@gmail.com>
2023-11-20 20:24:41 -0800

    image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,13 @@
+import img1 from './image/Untitled.png'
+
 export default function Home (){
-    return <h1>Home</h1>
+    return (
+        <div>
+            <img src={img1} alt="Bronchure" className="brochure-image" />
+            <div className="text-overlay">
+                <h1>Welcome to Our Brochure Website</h1>
+                <p>Discover amazing products and services that suit your needs.</p>
+            </div>
+        </div>
+    )
 }
